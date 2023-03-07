683e52d06debc6b5e37666c8321e1e62b6b9245c
G <mail@grahamirvine.com>
2023-03-07 16:40:56 -0700

    added partials for new and edit view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
 
     if @user.update(user_params)
-      redirect_to @user
+      redirect_to new_user_path
     else
       render :edit, status: :unprocessable_entity
     end
